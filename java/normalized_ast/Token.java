8e55dff6c2c27623035f6fe3a62c18c2de489f96
0xtooyama <45735463+0xtooyama@users.noreply.github.com>
2019-02-20 02:57:52 +0900

    fix a display content

--- java/normalized_ast/Token.java
+++ java/normalized_ast/Token.java
@@ -14,6 +14,6 @@ public class Token {
 
     public String toString() {
         String tname = ListLexer.tokenNames[type];
-        return "<'" + text + "'," + tname + ">";
+        return text;
     }
 }
